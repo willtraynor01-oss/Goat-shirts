1ed9980d04cb78e38f4004e16b89db7166f3eaf1
willtraynor01-oss <willtraynor01@gmail.com>
2026-08-04 14:15:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+ const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", () => {
+    if (navMenu.style.display === "flex") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "flex";
+        navMenu.style.flexDirection = "column";
+        navMenu.style.position = "absolute";
+        navMenu.style.top = "80px";
+        navMenu.style.left = "0";
+        navMenu.style.width = "100%";
+        navMenu.style.background = "#000";
+        navMenu.style.padding = "20px";
+        navMenu.style.gap = "20px";
+    }
+});
