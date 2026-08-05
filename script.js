275854a4e4d72ad9765786a041f786fa63653bef
willtraynor01-oss <willtraynor01@gmail.com>
2026-08-05 14:05:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
  const menuButton = document.getElementById("menuButton");
 const navMenu = document.getElementById("navMenu");
 
-menuButton.addEventListener("click", () => {
-    if (navMenu.style.display === "flex") {
-        navMenu.style.display = "none";
-    } else {
-        navMenu.style.display = "flex";
-        navMenu.style.flexDirection = "column";
-        navMenu.style.position = "absolute";
-        navMenu.style.top = "80px";
-        navMenu.style.left = "0";
-        navMenu.style.width = "100%";
-        navMenu.style.background = "#000";
-        navMenu.style.padding = "20px";
-        navMenu.style.gap = "20px";
-    }
-});
+if (menuButton && navMenu) {
+
+    menuButton.addEventListener("click", () => {
+
+        if (navMenu.style.display === "flex") {
+            navMenu.style.display = "none";
+        } else {
+            navMenu.style.display = "flex";
+            navMenu.style.flexDirection = "column";
+            navMenu.style.position = "absolute";
+            navMenu.style.top = "80px";
+            navMenu.style.left = "0";
+            navMenu.style.width = "100%";
+            navMenu.style.background = "#000";
+            navMenu.style.padding = "20px";
+            navMenu.style.gap = "20px";
+        }
+
+    });
+
 // Add to Cart
 const addToCartButton = document.getElementById("addToCart");
 
